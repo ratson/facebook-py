1fbcfa6781d2bcab8970fc86e4eb660917662b42
Ratson <github@ratson.name>
2017-01-28 00:18:17 +0800

    Rename package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup
  
 setup(
-    name='facebook-python-sdk',
+    name='facebook-py',
     version='0.1',
     description='This client library is designed to support the Facebook Graph API and the official Facebook JavaScript SDK, which is the canonical way to implement Facebook authentication.',
-    author='Facebook',
-    url='http://github.com/facebook/python-sdk',
+    author='Ratson',
+    url='https://github.com/ratson/facebook-py',
     package_dir={'': 'src'},
     py_modules=[
         'facebook',
